2898f4047dd332f2edea9548fd60aadb3fa222df
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-23 12:41:30 -0700

    use request npm package to retrieve Github API data

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,15 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
+let getReposByUsername = (username, callback) => {
   let options = {
-    url: 'FILL ME IN',
+    url: 'https://api.github.com/users/'+username+'/repos',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  request(options, callback);
 }
 
 module.exports.getReposByUsername = getReposByUsername;
