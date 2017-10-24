e25b9e5e90ae738f635d6fd2e4464c2a53b5c737
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-23 17:10:32 -0700

    add endpoint to handle POST requests for fetching  user data from Github API

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 let app = express();
+var bodyParser = require('body-parser');
 var database = require('../database/index.js');
+var githubHelper = require('../helpers/github.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  var username = req.body.username;
+  var retrievedRepos = githubHelper.getReposByUsername(username, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var info = JSON.parse(body);
+      database.save(info);
+    }
+  });
 });
 
+
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
   // This route should send back the top 25 repos
 });
 
